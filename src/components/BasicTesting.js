3952363036678059ee77ad0c3a3c9b7169b6624f
munikumarj <munikumardarling456@gmail.com>
2020-11-12 11:56:34 +0530

    testing onChange event

--- src/components/BasicTesting.js
+++ src/components/BasicTesting.js
@@ -1,9 +1,15 @@
-import React from 'react'
-
+import React,{useState} from 'react'
 function BasicTesting() {
+    const [input, setinput] = useState("")
+    const [output, setoutput] = useState("")
+    const changeHandular=(e)=>{
+     setinput(e.target.value)
+    }
     return (
         <div>
-            <h1 data-testId="h1Content">Hello there I'm back, and i'm came here with testing knowlwdge</h1>
+            <h1 aria-label="h1Content" className="tagh1">Hello there I'm back, and i'm here testing knowlwdge</h1>
+            <input placeholder="Enter Text" data-testid="inputTag" type="text" value={input} onChange={changeHandular} />
+            <input placeholder="OutPutTag" data-testid="outputTag" type="text" value={output} onChange={e=>setoutput(e.target.value)} />
         </div>
     )
 }
--- src/components/__test__/BasicTesting.test.js
+++ src/components/__test__/BasicTesting.test.js
@@ -1,5 +1,5 @@
 import React from "react"
-import {render} from "@testing-library/react"
+import {render, fireEvent, getByLabelText} from "@testing-library/react"
 import BasicTesting from "../BasicTesting"
 
 describe("all about h1 tag",()=>{
@@ -8,10 +8,34 @@ describe("all about h1 tag",()=>{
         expect(asFragment(<BasicTesting/>)).toMatchSnapshot()
     })
     test('should Have text content', () => {
-        const {getByTestId} = render(<BasicTesting/>)
-        expect(getByTestId("h1Content")).toHaveTextContent("Hello there I'm back, and i'm came here with testing knowlwdge")
-        
+        const {getByLabelText,getByTestId,queryByPlaceholderText} = render(<BasicTesting/>)
+        expect(getByLabelText("h1Content")).toHaveTextContent("Hello there I'm back, and i'm here testing knowlwdge")
+        expect(getByLabelText("h1Content")).toHaveClass("tagh1")
+        expect(getByTestId("inputTag")).toBeTruthy()
+        expect(queryByPlaceholderText("Enter Text")).toBeTruthy()
+
+    }) 
+    it ("input Text Testing",()=>{
+        const {queryByPlaceholderText} =render(<BasicTesting/>)
+        const InputTextValue = queryByPlaceholderText("Enter Text")
+
+        fireEvent.change(InputTextValue,{target:{value:"testing"}})
+
+        expect(InputTextValue.value).toBe("testing")
+    })     
+})
+describe("all about output tag",()=>{
+
+    test("should have placeholder",()=>{
+        const {getByPlaceholderText} =render(<BasicTesting/>)
+        expect(getByPlaceholderText("OutPutTag")).toBeTruthy()
     })
-    
-    
+    test("running Correctly",()=>{
+        const {getByTestId}=render(<BasicTesting/>)
+        const OutPutTesting=getByTestId("outputTag")
+        fireEvent.change(OutPutTesting,{target:{value:"outputcontent"}})
+        
+        expect(OutPutTesting.value).toBe("outputcontent")
+    })
+
 })
